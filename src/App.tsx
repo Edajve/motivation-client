b0e687cab91350dfb7b6dfb9faceb63c0c192660
Dajve Echols <dajvelechols@gmail.com>
2024-01-28 19:44:47 -0600

    reset template layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Box } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
 import './App.css';
-import YearProgress from './components/YearProgress';
 
 function App() {
   return (
     <>
-      <YearProgress />
+      <Grid
+        h='100vh'
+        w='100vw'
+        templateRows='repeat(2, 1fr)'
+        templateColumns='repeat(5, 1fr)'
+        gap={4}
+      >
+        <GridItem rowSpan={2} colSpan={1} bg='tomato' />
+        <GridItem colSpan={2} bg='papayawhip' />
+        <GridItem colSpan={2} bg='papayawhip' />
+        <GridItem colSpan={4} bg='tomato' />
+      </Grid>
     </>
   )
 }
