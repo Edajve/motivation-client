01401a8cf45630a9748d49a5a2c1a80c5a95b15a
Dajve Echols <dajvelechols@gmail.com>
2024-01-28 20:57:45 -0600

    clean up

---
+++ src/components/Quote.tsx
@@ -0,0 +1,13 @@
+import { Box, Text } from '@chakra-ui/react'
+
+function Quote() {
+    return (
+        <>
+            <Box id='quoteBox'>
+                <Text fontSize='3xl'>(3xl) In love with React & Next</Text>
+            </Box>
+        </>
+    )
+}
+
+export default Quote;
