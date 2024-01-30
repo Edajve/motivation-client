b5290164e1694bf94f387dfe972f6bbcd8fd497f
Dajve Echols <dajvelechols@gmail.com>
2024-01-30 00:41:15 -0600

    added delete book modal

--- src/components/DeleteABookButton.tsx
+++ src/components/DeleteABookButton.tsx
@@ -1,16 +1,55 @@
-import { DeleteIcon } from "@chakra-ui/icons";
+import { useRef } from 'react';
+import { DeleteIcon } from '@chakra-ui/icons';
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+    Button,
+    useDisclosure,
+} from '@chakra-ui/react';
 
 const DeleteABookButton = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const cancelRef = useRef<HTMLButtonElement | null>(null); // Corrected the type
 
-    const onDelete = () => {
-        console.log("send the id of the given element")
+    const onDeleteIcon = () => {
+        console.log("send/or get the id of the given element");
+        onOpen();
+    };
+
+    const onConfirmDelete = () => {
+        console.log('delete book by id')
+        onClose()
     }
 
     return (
         <>
-            <DeleteIcon onClick={() => onDelete()}/>
+            <DeleteIcon onClick={() => onDeleteIcon()} />
+            <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                            Delete Customer
+                        </AlertDialogHeader>
+                        <AlertDialogBody>
+                            Are you sure? You can't undo this action afterwards.
+                        </AlertDialogBody>
+                        <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button colorScheme='red' onClick={() => onConfirmDelete()} ml={3}>
+                                Delete
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
         </>
-    )
-}
+    );
+};
 
-export default DeleteABookButton
+export default DeleteABookButton;
